88d72023a0d3ee743d39dc211f2bcc73e3d19af9
AlbertoYabeda <albertoacmacuacua@gmail.com>
2021-07-20 12:49:21 +0100

    status page working fine

--- whatsapp_clone/lib/models/status_model.dart
+++ whatsapp_clone/lib/models/status_model.dart
@@ -1,4 +1,32 @@
 
 class StatusModel{
+    final String name;
+    final String timePosted;
+    final String imageUrl;
 
+    StatusModel({
+      required this.name,
+      required this.timePosted,
+      required this.imageUrl
+  });
 }
+
+List<StatusModel> randomStatus = [
+  new StatusModel(
+      name: "Algy Jr",
+      timePosted:"5m ago",
+      imageUrl: "https://avatars.githubusercontent.com/u/52712652?v=4"),
+  new StatusModel(
+      name: "Fernando Brther",
+      timePosted:"10m",
+      imageUrl: "https://png.pngtree.com/element_our/sm/20180511/sm_2efad18136c24c2f110ac98fd3d47b4f.jpg"),
+  new StatusModel(
+      name: "Puto Dalton",
+      timePosted:"19h",
+      imageUrl: "https://www.byri.net/wp-content/uploads/2021/03/New-BMW-M3-grows-to-600-horsepower-thanks-to-Manhart.jpg"),
+  new StatusModel(
+      name: "Maksel Drums",
+      timePosted:"23h",
+      imageUrl: "https://www.mad4wheels.com/img/free-car-images/mobile/17357/bmw-118i-f40-sportline-2019-546628.jpg"),
+];
+
--- whatsapp_clone/lib/pages/status_page.dart
+++ whatsapp_clone/lib/pages/status_page.dart
@@ -1,16 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/status_model.dart';
 
 class StatusPage extends StatelessWidget {
   const StatusPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        'No Status Available',
-        style: TextStyle(fontSize: 22.0, fontFamily: 'Roboto'),
-      ),
-    );
+    return new ListView.builder(
+      itemCount: randomStatus.length,
+        itemBuilder: (context,index)=> (
+
+        Column(
+          children: <Widget>[
+            new Padding(padding: EdgeInsets.only(top: 1)),
+            new Divider(
+              height: 3.0,
+              indent: 80,
+              color: Colors.grey,
+            ),
+            ListTile(
+              leading: new CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.blue,
+                child: new CircleAvatar(
+                    radius: 26,
+                    foregroundColor: Theme.of(context).accentColor,
+                    foregroundImage: NetworkImage(randomStatus[index].imageUrl),
+                ),
+              ),
+              title: Text(randomStatus[index].name),
+              subtitle: Text(randomStatus[index].timePosted),
+            )
+          ],
+        )
+        ),);
   }
 }
 
